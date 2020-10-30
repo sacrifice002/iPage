4c7f417c391a64ac1d4e595ad4c5b0fd53af1955
谢于中 <yuzhong.xie@za.group>
2020-10-30 11:16:09 +0800

    feat: 用户停留时间 init

--- src/pages/dataView/index.js
+++ src/pages/dataView/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import StayTime from './components/staryTime'
+import style from './index.module.scss'
+import ChartContainer from './components/chartContainer'
 
-function DataView(){
-    return (
-        <div>
-            这里是数据可视化页面
-        </div>
-    )
+function DataView() {
+  return (
+    <div className={style.dataViewContainer}>
+      <ChartContainer title='用户停留时间'>
+        <StayTime />
+      </ChartContainer>
+    </div>
+  )
 }
 
 export default DataView
